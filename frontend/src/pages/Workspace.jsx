a20292bce8d076d2cde5264aff657d3a8f203f07
Sai Vishwak Jadala <saivishwakjadala2001@gmail.com>
2026-08-06 12:10:34 -0500

    Workspace UI

--- frontend/src/pages/Workspace.jsx
+++ frontend/src/pages/Workspace.jsx
@@ -59,7 +59,7 @@ export default function Workspace() {
 
                     <WorkspaceMembers />
 
-                    <WorkspaceActivity />
+                    {/*<WorkspaceActivity />*/}
 
                 </Grid>
 
